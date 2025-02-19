990e440323d6a0140cf58d2201f9dd049e4e4158
Kanmanemone <aa20438272@gmail.com>
2025-02-19 09:34:06 +0900

    임시

--- app/src/main/java/com/example/nutri_capture_new/MainActivity.kt
+++ app/src/main/java/com/example/nutri_capture_new/MainActivity.kt
@@ -6,11 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -28,26 +31,28 @@ class MainActivity : ComponentActivity() {
     // nutrientViewModel이 Hilt의 viewModels()에 의해 관리되도록 위임(by). 따라서 반드시 by 키워드로 선언해야함.
     private val nutrientViewModel: NutrientViewModel by viewModels()
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         enableEdgeToEdge()
         setContent {
             NutricapturenewTheme {
-                Scaffold(
+                BottomSheetScaffold(
                     modifier = Modifier
                         .fillMaxSize()
                         .windowInsetsPadding(WindowInsets.ime),
-                    bottomBar = {
-                        NutrientChatBar()
+                    sheetContent = {
+
                     }
                 ) { innerPadding ->
-                    Box(
+                    Column(
                         modifier = Modifier
                             .padding(innerPadding)
                             .fillMaxSize()
                     ) {
                         NutrientScreen()
+                        NutrientChatBar()
                     }
                 }
             }
