9fa8416aa8e07e42f572ba2c96521611df90653c
Kanmanemone <aa20438272@gmail.com>
2025-03-25 18:59:41 +0900

    '피자' 아이콘 임시 적용 (https://kenel.tistory.com/323)

--- app/src/main/java/com/example/nutri_capture_new/nutrient/NutrientBottomSheet.kt
+++ app/src/main/java/com/example/nutri_capture_new/nutrient/NutrientBottomSheet.kt
@@ -1,40 +1,68 @@
 package com.example.nutri_capture_new.nutrient
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.sharp.Done
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.nutri_capture_new.db.DayMeal
+import com.example.nutri_capture_new.db.NutritionInfo
+import com.example.nutri_capture_new.utils.ResponsiveArcSurroundedIconButton
 
 @Composable
 fun NutrientBottomSheet(
     viewModel: NutrientViewModel = hiltViewModel()
 ) {
-    Column(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        SampleTexts()
-    }
+    val inputtedDayMeal = viewModel.nutrientScreenState.collectAsState().value.inputtedDayMeal
+
+    NutrientInputKeyboard(
+        modifier = Modifier.fillMaxSize(), inputtedDayMeal = inputtedDayMeal
+    )
 }
 
 @Composable
-fun SampleTexts(
+fun NutrientInputKeyboard(
     modifier: Modifier = Modifier,
-    fontSize: TextUnit = 16.sp,
+    inputtedDayMeal: DayMeal,
 ) {
-    val textCount = 30
-    repeat(textCount) {
-        Text(
-            text = "Item (${it + 1} / ${textCount})",
-            modifier = modifier,
-            fontSize = fontSize,
-        )
-        Spacer(modifier = Modifier.height(24.dp))
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(minSize = 100.dp),
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(12.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        items(
+            11 // TODO: inputtedDayMeal.nutritionInfo의 각 프로퍼티만큼 item 생성
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .border(width = 1.dp, color = Color.Black),
+                contentAlignment = Alignment.Center
+            ) {
+                ResponsiveArcSurroundedIconButton(
+                    imageVector = Icons.Sharp.Done,
+                    contentDescription = "test",
+                    currentLevel = 2,
+                    maxLevel = 3,
+                    arcColor = Color.Yellow,
+                    arcWidth = 15
+                ) {
+                    // TODO: inputtedDayMeal의 값을 변경하는 event
+                }
+            }
+        }
     }
 }
