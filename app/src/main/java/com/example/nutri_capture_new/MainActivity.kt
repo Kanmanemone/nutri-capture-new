313c8db4f135a153b0cf38a0b5558cac93e1000d
Kanmanemone <aa20438272@gmail.com>
2025-10-11 13:27:46 +0900

    '앱 진입 지점(Entry point)'의 스타일을 Now in Android와 같게 만듦

--- app/src/main/java/com/example/nutri_capture_new/MainActivity.kt
+++ app/src/main/java/com/example/nutri_capture_new/MainActivity.kt
@@ -5,30 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.ime
-import androidx.compose.foundation.layout.navigationBars
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBars
-import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.example.designsystem.NutricapturenewTheme
-import com.example.nutrient.NutrientBottomSheet
-import com.example.nutrient.NutrientChatBar
-import com.example.nutrient.NutrientScreen
+import com.example.nutri_capture_new.ui.NcApp
 import com.example.nutrient.NutrientViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    // nutrientViewModel이 Hilt의 viewModels()에 의해 관리되도록 위임(by). 따라서 반드시 by 키워드로 선언해야함.
     private val nutrientViewModel: NutrientViewModel by viewModels()
 
     @OptIn(ExperimentalMaterial3Api::class)
@@ -38,30 +23,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NutricapturenewTheme {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .windowInsetsPadding(WindowInsets.systemBars)
-                        .windowInsetsPadding(WindowInsets.navigationBars)
-                        .windowInsetsPadding(WindowInsets.ime),
-                ) {
-                    BottomSheetScaffold(
-                        sheetContent = {
-                            NutrientBottomSheet()
-                        },
-                        modifier = Modifier.fillMaxSize(),
-                        sheetPeekHeight = 300.dp
-                    ) { innerPadding ->
-                        Column(
-                            modifier = Modifier
-                                .padding(innerPadding)
-                                .fillMaxSize()
-                        ) {
-                            NutrientScreen()
-                            NutrientChatBar()
-                        }
-                    }
-                }
+                NcApp()
             }
         }
     }
