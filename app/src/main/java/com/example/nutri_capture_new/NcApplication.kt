6f6d9a6e5d17f773292a9e083489016a09becf5c
Kanmanemone <aa20438272@gmail.com>
2025-10-11 13:27:46 +0900

    [리팩토링] '앱 진입 지점(Entry point)'의 스타일을 Now in Android와 같게 만듦

--- app/src/main/java/com/example/nutri_capture_new/NcApplication.kt
+++ app/src/main/java/com/example/nutri_capture_new/NcApplication.kt
@@ -1,7 +1,7 @@
-package com.example.nutri_capture_new.di
+package com.example.nutri_capture_new
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class HiltApplication : Application()
+class NcApplication : Application()
---
+++ app/src/main/java/com/example/nutri_capture_new/ui/NcApp.kt
@@ -0,0 +1,11 @@
+package com.example.nutri_capture_new.ui
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import com.example.nutrient.NutrientScreen
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NcApp() {
+    NutrientScreen()
+}
